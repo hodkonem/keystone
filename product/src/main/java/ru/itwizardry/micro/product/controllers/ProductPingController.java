c0351de0294624f5117851d12d2bd13f8ef0e08c
Misha <mikhaillat@yandex.ru>
2025-04-12 01:02:39 +0300

    Исправил Dockerfile-ы и docker-compose.yml привел настройки и контроллеры к общему формату. Теперь соответствует критериям приемки stage1

--- product/src/main/java/ru/itwizardry/micro/product/controllers/ProductPingController.java
+++ product/src/main/java/ru/itwizardry/micro/product/controllers/ProductPingController.java
@@ -2,18 +2,16 @@ package ru.itwizardry.micro.product.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
-public class ProductController {
+public class ProductPingController {
 
     @Value("${spring.application.name}")
     private String appName;
 
-    @GetMapping("/product")
-    public String testPort() {
-        return "product";
+    @GetMapping("/ping")
+    public String ping() {
+        return "OK from " + appName;
     }
 }
