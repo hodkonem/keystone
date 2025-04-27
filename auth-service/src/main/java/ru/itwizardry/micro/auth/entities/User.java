c3f042847c2080ade4df6308c01ff55222f3028f
Misha <mikhaillat@yandex.ru>
2025-04-27 08:34:55 +0300

    Добавил Lombok в auth-service. Модифицировал сущность User, добавил новые поля

--- auth-service/src/main/java/ru/itwizardry/micro/auth/entities/User.java
+++ auth-service/src/main/java/ru/itwizardry/micro/auth/entities/User.java
@@ -1,85 +1,73 @@
 package ru.itwizardry.micro.auth.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import ru.itwizardry.micro.auth.converter.RoleConverter;
 
+import java.time.Instant;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, length = 255)
+    @EqualsAndHashCode.Include
     private String username;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 255)
     private String password;
+
+    @Email
+    @Column(length = 255)
+    private String email;
+
+    @Pattern(regexp = "^\\+?[0-9\\s()-]{7,20}$")
+    @Column(length = 20)
+    private String phone;
+
+    private String address;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
 
     @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     @Column(name = "role")
     @Convert(converter = RoleConverter.class)
+    @Builder.Default
     private Set<Role> roles = new HashSet<>();
 
-    public User() {
+        public void addRole(Role role) {
+        this.roles.add(role);
     }
 
-    public User(String username, String password, Set<Role> roles) {
-        this.username = username;
-        this.password = password;
-        this.roles = roles;
+    public void removeRole(Role role) {
+        this.roles.remove(role);
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(username, user.username) &&
-                Objects.equals(password, user.password) && Objects.equals(roles, user.roles);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, username, password, roles);
+    public boolean hasRole(Role role) {
+        return this.roles.contains(role);
     }
 }
-
-
