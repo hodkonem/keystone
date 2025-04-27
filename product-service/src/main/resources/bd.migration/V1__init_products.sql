42340b5f697bc2c8bf160304df3076351863a0eb
Misha <mikhaillat@yandex.ru>
2025-04-27 14:24:30 +0300

    Изменил pom.xml в auth-service и в product-service. Добавил миграции V1__init_products.sql и V2__add_test_products.sql в product-service

--- product-service/src/main/resources/bd.migration/V1__init_products.sql
+++ product-service/src/main/resources/bd.migration/V1__init_products.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS products
 (
     id BIGSERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
+    description TEXT NOT NULL,
     price FLOAT NOT NULL,
     quantity INT NOT NULL
 );
